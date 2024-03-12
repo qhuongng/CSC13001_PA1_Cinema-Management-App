23884053160693f23470860654d38f4811a5cefa
qhuongng <nq.huong.2304@gmail.com>
2024-03-12 14:14:27 +0700

    Revise UI and add some data binding

--- CineManagement/Home.xaml.cs
+++ CineManagement/Home.xaml.cs
@@ -1,16 +1,29 @@
 ﻿using System.Windows.Controls;
+using CineManagement.Models;
+using CineManagement.Services;
 
 namespace CineManagement
 {
-    /// <summary>
-    /// Interaction logic for Home.xaml
-    /// </summary>
     public partial class Home : UserControl
     {
+        List<Movie> movies = new List<Movie>();
+        List<Byte[]> posters = new List<Byte[]>();
+
+        MovieService ms = new MovieService();
+
         public Home()
         {
             InitializeComponent();
             DataContext = this;
+            
+            for (int i = 0; i < 8; i++)
+            {
+                movies.Add(ms.getMovieById(i + 1));
+                posters.Add(ms.getMovieById(i + 1).Poster);
+            }
+
+            movieList.ItemsSource = movies;
+            posterBanner.ItemsSource = posters;
         }
     }
 }
